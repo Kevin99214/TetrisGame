6573ea5df7dd6a34c94f512bd46e24e5edb826b3
Kevin Qin <kevinqin99214@gmail.com>
2019-07-19 22:17:30 -0400

    changed camera speed and size depending on height of tower

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class CameraControl : MonoBehaviour
 {
     //constants
-    private Vector3 CAMERA_SPEED = new Vector3(0, 0.075f, 0);
+    private Vector3 CAMERA_SPEED = new Vector3(0, 0.05f, 0);
     //starting height of camera
     private const float CAMERA_START_HEIGHT = -1;
     //height range
@@ -15,9 +15,9 @@ public class CameraControl : MonoBehaviour
     //0 represents default/reset value
     //1 represents something has been hit
     //2 represents nothing has been hit
-    private int hitCollider = 0;
-    //store raycast results
-    private RaycastHit2D hit;
+    // private int hitCollider = 0;
+    // //store raycast results
+    // private RaycastHit2D hit;
 
     // Start is called before the first frame update
     void Start()
@@ -28,58 +28,75 @@ public class CameraControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        //create ray to find tallest point
-        hit = Physics2D.Raycast(new Vector2(-20, GameBoard.Height), Vector2.right);
+        // //create ray to find tallest point
+        // hit = Physics2D.Raycast(new Vector2(-20, GameBoard.Height), Vector2.right);
 
-        //if collider hit
-        if (hit.collider != null)
+        // //if collider hit
+        // if (hit.collider != null)
+        // {
+        //     //if previous check showed no collider but now collider hit
+        //     //found highest point, update camera position
+        //     if (hitCollider == 2)
+        //     {
+        //         updateCamera();
+        //     }
+        //     else
+        //     {
+        //         hitCollider = 1;
+        //         GameBoard.Height = GameBoard.round(GameBoard.Height + 0.5f, 0.5f);
+        //     }
+        // }
+        // else
+        // {
+        //     //if previous point showed collider hit but now no collider hit
+        //     //found highest point, update camera position
+        //     if (hitCollider == 1)
+        //     {
+        //         updateCamera();
+        //     }
+        //     else
+        //     {
+        //         hitCollider = 2;
+        //         GameBoard.Height = GameBoard.round(GameBoard.Height - 0.5f, 0.5f);
+        //     }
+        // }
+
+        updateCamera();
+
+        // if block is higher than what camera can see, increase size of camera
+        // Debug.Log("Height: " + GameBoard.Height + " Camera: " + (Camera.main.transform.position.y + Camera.main.orthographicSize));
+        if (GameBoard.Height > (Camera.main.transform.position.y + Camera.main.orthographicSize))
         {
-            //if previous check showed no collider but now collider hit
-            //found highest point, update camera position
-            if (hitCollider == 2)
-            {
-                updateCamera();
-            }
-            else
-            {
-                hitCollider = 1;
-                GameBoard.Height = GameBoard.round(GameBoard.Height + 0.5f, 0.5f);
-                //Debug.Log(GameBoard.getHeight());
-            }
+            Camera.main.orthographicSize += CAMERA_SPEED.y;
         }
-        else
+        //camera can now see blocks, shrink camera back to default
+        else if (Camera.main.orthographicSize > GameBoard.cameraSize)
         {
-            //if previous point showed collider hit but now no collider hit
-            //found highest point, update camera position
-            if (hitCollider == 1)
-            {
-                updateCamera();
-            }
-            else
-            {
-                hitCollider = 2;
-                GameBoard.Height = GameBoard.round(GameBoard.Height - 0.5f, 0.5f);
-                //Debug.Log(GameBoard.getHeight());
-            }
+            Camera.main.orthographicSize -= CAMERA_SPEED.y;
         }
     }
 
     void updateCamera()
     {
+        if (GameBoard.TempHeight != GameBoard.Height)
+        {
+            GameBoard.Height = GameBoard.TempHeight;
+        }
+        GameBoard.TempHeight = -10f;
         //keep camera at starting height until blocks reach above 0
         if (GameBoard.Height > (CAMERA_START_HEIGHT))
         {
             //if camera is below max height and delta height is within range
             //move camera up
-            if ((transform.position.y < GameBoard.Height + 1) &&
-                (Mathf.Abs(transform.position.y - GameBoard.Height - 1) > HEIGHT_RANGE))
+            if ((transform.position.y < GameBoard.Height) &&
+                (Mathf.Abs(transform.position.y - GameBoard.Height) > HEIGHT_RANGE))
             {
                 transform.position += CAMERA_SPEED;
             }
             //if camera is above max height and delta height is within range
             //move camera up
-            else if ((transform.position.y > GameBoard.Height + 1) &&
-                (Mathf.Abs(transform.position.y - GameBoard.Height - 1) > HEIGHT_RANGE))
+            else if ((transform.position.y > GameBoard.Height) &&
+                (Mathf.Abs(transform.position.y - GameBoard.Height) > HEIGHT_RANGE))
             {
                 transform.position -= CAMERA_SPEED;
             }
